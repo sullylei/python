2b5a5e34116ebe2c0fab04c7e12bfd0aa6cc4dd9
sully <270040656@qq.com>
2020-05-22 00:22:10 +0800

    代理

---
+++ xiaojiayu_rumen/web_crawler/03proxy_eg.py
@@ -0,0 +1,16 @@
+import urllib.request
+import random
+
+url = 'http://www.whatismyip.com.tw'
+
+iplist = ['119.6.144.73:81','183.203.208.166:8118','111.1.32.28:81']
+proxy_support = urllib.request.ProxyHandler({'http':random.choice(iplist)})
+
+opener = urllib.request.build_opener(proxy_support)
+
+urllib.request.install_opener(opener)
+
+response = urllib.request.urlopen(url)
+html = response.read().decode('utf-8')
+
+print(html)
